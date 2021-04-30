285c97190b438cd7c0bd337723e22d19619c1e6f
shunak <shuna966@gmail.com>
2021-04-30 21:21:52 +0900

    schema

---
+++ server/schema/schema.js
@@ -0,0 +1,29 @@
+// schema represents for specification of graphql api
+const graphql = require("graphql"); // create graphql object
+// Get graphql oject type from graphql object
+const { GraphQLObjectType, GraphQLID, GraphQLString } = graphql;
+
+// GraphQLObjectType is a format for Type. it enables creating instance.
+const MovieType = new GraphQLObjectType({
+  name: "Movie",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString },
+  }),
+});
+
+
+// RootQuery : For external accessor
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        movie: {
+            type: MovieType,
+            args: { id: { type: GraphQLString } },
+            resolve(parents, args) {
+                
+            }
+        }
+    }
+})
