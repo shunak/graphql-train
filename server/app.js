552d5642d0cce550f8b512d3acd84de8fc020f87
shunak <shuna966@gmail.com>
2021-05-01 21:31:09 +0900

    added mutation

--- server/app.js
+++ server/app.js
@@ -1,15 +1,26 @@
-const express = require("express")
+const express = require("express");
 
-const graphqlHTTP = require('express-graphql') // create graphqlHTTP object
+const { graphqlHTTP } = require("express-graphql"); // create graphqlHTTP object
+const mongoose = require("mongoose"); // create mongoose object which has connect mehtod for mongoDB
 
-const app = express()
+const schema = require('./schema/schema') // Read schema from schema.js
+
+const app = express();
+mongoose.connect(
+  "mongodb+srv://admin:admin@cluster0.hlcut.mongodb.net/test?retryWrites=true&w=majority"
+);
+mongoose.connection.once("open", () => {
+  console.log("db connected");
+});
 
 // To create middleware, use use method of app object
 // first arg = , second argument = handler function of middleware
-app.use('/graphql',graphqlHTTP({
+app.use("/graphql", graphqlHTTP({
+    schema, // add schema to middleware
+    graphiql: true // graphql test tool, enables API testing only in backend
+}));
 
-}))
 
 app.listen(4000, () => {
-    console.log('listening port 4000')
-})
+  console.log("listening port 4000");
+});
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,9 @@
 // Schema represents for specification of graphql api
 const graphql = require("graphql"); // create graphql object
+const movie = require("../models/movie");
+
+const Movie = require("../models/movie");
+
 // Get graphql oject type from graphql object
 const { GraphQLObjectType, GraphQLID, GraphQLString } = graphql;
 
@@ -13,17 +17,46 @@ const MovieType = new GraphQLObjectType({
   }),
 });
 
-
 // RootQuery : For external accessor
 const RootQuery = new GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: {
-        movie: {
-            type: MovieType,
-            args: { id: { type: GraphQLString } },
-            resolve(parents, args) {
-                
-            }
-        }
-    }
-})
+  name: "RootQueryType",
+  fields: {
+    movie: {
+      type: MovieType,
+      args: { id: { type: GraphQLString } },
+      resolve(parents, args) {
+        // Getting data process from mongoDB
+        return Movie.findById(args.id);
+      },
+    },
+  },
+});
+
+// Mutation object
+const Mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    addMovie: {
+      type: MovieType,
+      args: {
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString },
+      },
+      resolve(parent, args) {
+        // resolve function: create moive object by using model
+        let movie = new Movie({
+          name: args.name,
+          genre: args.genre,
+        });
+
+        return movie.save();
+      },
+    },
+  },
+});
+
+// module is exported to app.js
+module.exports = new graphql.GraphQLSchema({
+  query: RootQuery,
+  mutation: Mutation,
+});
